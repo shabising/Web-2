a0b13db78abd900f05021df2c61751642f02d069
Shabnam Osmanova <shabismanova127@gmail.com>
2026-04-13 01:58:42 +0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,9 @@
 -- CREATE DATABASE ecommerce_db;
--- USE ecommerce_db;
+USE ecommerce_db;
+
 -- CREATE TABLE categories (
---  id INT AUTO_INCREMENT PRIMARY KEY,
---    name VARCHAR(100) NOT NULL
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL
 -- );
 
 -- CREATE TABLE products (
@@ -13,15 +14,12 @@
 --     FOREIGN KEY (category_id) REFERENCES categories(id)
 -- );
 
--- INSERT INTO categories (name)
--- VALUES ('Phones'), ('Laptops'), ('Tablets');
 
--- INSERT INTO products (name, price, category_id)
--- VALUES
--- ('iPhone 14', 1800, 1),
--- ('Samsung Galaxy S23', 1500, 1),
--- ('Lenovo ThinkPad', 2200, 2);
+INSERT INTO categories (name)
+VALUES ('Phones'), ('Laptops'), ('Tablets');
 
-
-
-
+INSERT INTO products (name, price, category_id)
+VALUES
+('iPhone 14', 1800, 1),
+('Samsung Galaxy S23', 1500, 1),
+('Lenovo ThinkPad', 2200, 2);
